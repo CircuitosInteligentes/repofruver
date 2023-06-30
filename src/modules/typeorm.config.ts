550d60a1af8010215cd71ecf0a9c8d4c78b71ec6
CircuitosInteligentes <repocircuitosinteligentes@gmail.com>
2023-06-30 19:52:06 -0400

    Configurando config.ts 1.1

--- src/modules/typeorm.config.ts
+++ src/modules/typeorm.config.ts
@@ -2,12 +2,14 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const config: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'dreamins_repocircuitosint',
-  password: 'M^QJjWde*g@1GUqm',
-  database: 'dreamins_inventario',
-
+  host: process.env.DB_HOST || 'containers-us-west-192.railway.app',
+  port: Number.parseInt(process.env.DB_PORT) || 7362,
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'gFFdMTGQfi9q9JG0uinY',
+  database: process.env.DB_NAME || 'railway',
+  ssl: {
+    rejectUnauthorized: false,
+  },
   entities: ['dist/**/*.entity{.ts,.js}'],
   synchronize: true,
 };
