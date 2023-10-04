c6728e4ffc55ab8facdd2a8103903d76dec4f1ab
CircuitosInteligentes <repocircuitosinteligentes@gmail.com>
2023-10-04 12:27:59 -0300

    Modificando configuración proyecto json

--- src/modules/typeorm.config.ts
+++ src/modules/typeorm.config.ts
@@ -2,13 +2,14 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const config: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: process.env.DB_HOST || 'containers-us-west-192.railway.app',
-  port: Number.parseInt(process.env.DB_PORT) || 7362,
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'gFFdMTGQfi9q9JG0uinY',
-  database: process.env.DB_NAME || 'railway',
+  host: process.env.DB_HOST || 'database-fruver.cserjyebsqnn.us-east-2.rds.amazonaws.com',
+  port: Number.parseInt(process.env.DB_PORT) || 3306,
+  username: process.env.DB_USER || 'circuitos_aws',
+  password: process.env.DB_PASSWORD || 'juanito21A#',
+  database: process.env.DB_NAME || 'db-fruver',
   ssl: {
     rejectUnauthorized: false,
+
   },
   entities: ['dist/**/*.entity{.ts,.js}'],
   synchronize: true,
